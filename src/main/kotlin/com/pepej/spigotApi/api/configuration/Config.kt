ad01bf6408a373a7b13abb29114c31881d3c9972
PepejHa4ker <48170835+PepejHa4ker@users.noreply.github.com>
2020-06-23 21:09:25 +1000

    Update Config.kt

--- src/main/kotlin/com/pepej/spigotApi/api/configuration/Config.kt
+++ src/main/kotlin/com/pepej/spigotApi/api/configuration/Config.kt
@@ -29,7 +29,7 @@ abstract class Config(
         try { save(File(plugin.dataFolder, fileName)) } catch (ignored: Exception) {}
 
     }
-    inline fun <reified R> withConfig(block: Config.() -> R): R {
+    inline fun <R> withConfig(block: Config.() -> R): R {
         return block()
     }
 
